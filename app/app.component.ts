fd46f842d71b909191ab967c53ed000785a6d592
NajibOsman <machidydemajka38@gmail.com>
2017-06-02 16:59:12 +0300

    modified the app.component

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
   <div class="container">
-    <h1>My First Angular 2 App</h1>
+    <h1>My Meal Tracker</h1>
     <div *ngFor="let currentTask of tasks">
       <h3 >{{ currentTask.description }}</h3>
       <button (click)="showDetails(currentTask)" >Edit</button>
@@ -12,11 +12,11 @@ import { Component } from '@angular/core';
     <div *ngIf="selectedTask">
       <h1>Edit Task</h1>
       <div>
-        <label>Enter Task Description:</label>
+        <label>Enter The Details:</label>
         <input [(ngModel)]="selectedTask.description">
       </div>
       <div>
-        <label>Enter Task ID:</label>
+        <label>calories:</label>
         <input [(ngModel)]="selectedTask.id">
         <button (click)="finishedEditing()">Done</button>
       </div>
@@ -27,10 +27,10 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   public tasks: Task[] = [
-      new Task("Create To-Do List app.", 0),
-      new Task("Learn Kung Fu.", 1),
-      new Task("Rewatch all the Lord of the Rings movies.", 2),
-      new Task("Do the laundry.", 3)
+      new Task("Humbuger.", 0),
+      new Task("Roasted me.", 1),
+      new Task("Fries.", 2),
+      new Task("Chicken Burger.", 3)
   ];
   selectedTask: Task = null;
   showDetails(clickedTask: Task){
